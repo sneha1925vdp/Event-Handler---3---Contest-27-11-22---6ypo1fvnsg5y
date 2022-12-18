29b35f50a047f1f8635f1a08f860823912db8157
sneha1925vdp <55706754+sneha1925vdp@users.noreply.github.com>
2022-12-19 01:21:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
+
+  const [updateText, setUpdateText] = useState("");
+  const [updateNum, setUpdateNum] = useState("");
 
   const handleInput = (event) =>{
    // use console.log
     let txt = event.target.value;
-   console.log("#text-input is", txt);
+    setUpdateText(txt);
+   console.log("Input in #text-input is ", txt);
 
   }
 
   const handleInput1 = (event) =>{
     // use console.log
      let num = event.target.value;
-    console.log("#num-input 1 is", num.toString());
+    setUpdateNum(num);
+    console.log("Input in #num-input is", num.toString());
  
    }
 
@@ -20,13 +25,13 @@ const App = () => {
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" onChange={handleInput} type={'text'} />
+      <input id="text-input" onChange={handleInput} type={'text'} value={updateText}/>
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input" type={'number'} onChange={handleInput1}/>
+      <input id="num-input" type={'number'} onChange={handleInput1} value={updateNum}/>
       <br/>
     </div>
   )
